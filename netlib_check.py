ce4a63135b130b978d5867c2cba29c8e01414817
linhht <31060606+linhht@users.noreply.github.com>
2019-03-18 17:25:43 +0700

    Update netlib_check.py

--- netlib_check.py
+++ netlib_check.py
@@ -46,18 +46,18 @@ def check_bgp_status():
     #sys.exit()
     
 ############################## Main program ###############################
-    while (True):
-        print('\n\n##################################################')
-        print('1-Check interface status')
-        print('2-Check BGP status')
-        print('3-Exit')
-        print('##################################################\n\n')
-        prompt = input('Please select from above option: ')
-        if (prompt == "1"):
-            print('1-Proceed Check interface status \n\n')
-            #check_And_Change()
-        elif (prompt == "2"):
-            print('2-Check BGP status \n\n')
-            check_bgp_status()
-        elif (prompt == "3"):
-            break
+while (True):
+       print('\n\n##################################################')
+       print('1-Check interface status')
+       print('2-Check BGP status')
+       print('3-Exit')
+       print('##################################################\n\n')
+       prompt = input('Please select from above option: ')
+       if (prompt == "1"):
+           print('1-Proceed Check interface status \n\n')
+           #check_And_Change()
+       elif (prompt == "2"):
+           print('2-Check BGP status \n\n')
+           check_bgp_status()
+       elif (prompt == "3"):
+           break
