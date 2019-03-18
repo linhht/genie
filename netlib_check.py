c7f550591b806feb7fb986a14cc485e869719b6c
linhht <31060606+linhht@users.noreply.github.com>
2019-03-18 17:14:27 +0700

    Update netlib_check.py

--- netlib_check.py
+++ netlib_check.py
@@ -12,23 +12,26 @@ from ats.topology import loader
 from genie.conf import Genie
 
 ############################## Define global variables ###############################
-testbed = loader.load('/root/workspace/devnet-2595/workshop/genie/my_testbed.yaml')
-genie_testbed = Genie.init(testbed)
-logfile = open('log_netlib_check.txt', 'w+')
+testbed = loader.load('/root/workspace/devnet-2595/workshop/genie/my_testbed.yaml') #location of testbed file
+genie_testbed = Genie.init(testbed) #load testbed file to genie
+logfile = open('log_netlib_check.txt', 'w+')    #open logfie
 
 ############################## Define functions ###############################
 def check_bgp_status():
     ###Check input argument
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2:   #if there is NO hostname arg
         print('Please input the device name')
         print('Usage: python netlib_check.py <device_hostname>')
         sys.exit()    
-    dev = ' '.join(sys.argv[1:])
+    dev = ' '.join(sys.argv[1:])    #if there is hostname, put it in 'dev' variable
     print('Get BGP status of ' + str(dev) + '\n')
     logfile.write('Get BGP status of ' + str(dev) + '\n')
-    device = genie_testbed.devices[str(dev)]
-    device.connect()
-    output = device.parse('show bgp all sum')
+    device = genie_testbed.devices[str(dev)]    #get device from testbed file
+    device.connect()    #connect to device
+    output = device.parse('show bgp all summary')   #parse command 'show bgp all summary' to 'output' variable
+    ### loop thru neighbors in output & check number of received prefixes
+    ### if number of received prefixes is integer --> neighbor is OK
+    ### if number of received prefixes is NOT integer --> neighbor is NOK
     for k,v in output['vrf']['default']['neighbor'].items():
         if v['address_family']['ipv4 unicast']['state_pfxrcd'].isdigit():
             print('Neighbor: ' + k + 'is UP and receive ' + str(v['address_family']['ipv4 unicast']['state_pfxrcd']) + ' prefixes')
