7c8cf4c5ae7e994029c28e856dc72986d04deb4c
linhht <31060606+linhht@users.noreply.github.com>
2019-03-26 11:58:49 +0700

    Update getConfigMultiDevice.py

--- getConfigMultiDevice.py
+++ getConfigMultiDevice.py
@@ -11,12 +11,26 @@ import sys
 import pprint
 from ats.topology import loader
 from genie.conf import Genie
-from getConfigSingleDevice import getconfig
 
 #define global variables
 testbed = loader.load('my_testbed.yaml')
 genie_testbed = Genie.init(testbed)
 logfile = open('log.txt', 'w+')
+
+#Define getconfig() function
+def getconfig(dev):
+    print('Get config of ' + str(dev) + '\n')               #Print out the msg on terminal
+    logfile.write('Get config of ' + str(dev) + '\n')       #Write msg to log file
+    device = genie_testbed.devices[str(dev)]                #Get device from genie_testbed var
+    device.connect()                                        #Connect to device
+    output_pre = device.parse('show running-config')        #Parse 'show run' cmd & store to outout_pre structure object
+    with open('config/running-config_' + str(device.name) + '.txt', 'wb') as f:
+        pickle.dump(output_pre, f)                          #Write outout_pre structure object to txt file use pickle
+    f.close()
+    logfile.write('Get config of ' + str(dev) + '---DONE!!!' + '\n')
+    logfile.close()                                         #Close log file
+    device.disconnect()                                     #Disconnect from device
+    sys.exit()                                              #Exist program
 
 #Main program
 if len(sys.argv) > 1:                                       #If user do enter more than one argument
@@ -35,4 +49,3 @@ for dev in genie_testbed.devices.values():
                 continue
 logfile.close()
 sys.exit()
-
