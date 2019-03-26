b81236f8f5392debffcd0c923b5c0fa6de08b521
linhht <31060606+linhht@users.noreply.github.com>
2019-03-26 11:28:38 +0700

    Create getconfig.py

---
+++ getconfig.py
@@ -0,0 +1,21 @@
+#Import neccessary libraries/modules
+import pickle
+import sys
+import pprint
+from ats.topology import loader
+from genie.conf import Genie
+
+#Define getconfig() function
+def getconfig(dev):
+    print('Get config of ' + str(dev) + '\n')               #Print out the msg on terminal
+    logfile.write('Get config of ' + str(dev) + '\n')       #Write msg to log file
+    device = genie_testbed.devices[str(dev)]                #Get device from genie_testbed var
+    device.connect()                                        #Connect to device
+    output_pre = device.parse('show running-config')        #Parse 'show run' cmd & store to outout_pre structure object
+    with open('config/running-config_' + str(device.name) + '.txt', 'wb') as f:
+        pickle.dump(output_pre, f)                          #Write outout_pre structure object to txt file use pickle
+    f.close()
+    logfile.write('Get config of ' + str(dev) + '---DONE!!!' + '\n')
+    logfile.close()                                         #Close log file
+    device.disconnect()                                     #Disconnect from device
+    sys.exit()                                              #Exist program
