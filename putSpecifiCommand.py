4ca8c91556ac213e0be873c49918ec7cfb4a66f1
linhht <31060606+linhht@users.noreply.github.com>
2019-03-26 16:04:06 +0700

    Update putSpecifiCommand.py

--- putSpecifiCommand.py
+++ putSpecifiCommand.py
@@ -1,27 +1,13 @@
-Skip to content
- 
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@linhht Sign out
-1
-0 0 linhht/genie
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights  Settings
-genie/putCommand.py
-@linhht linhht Update putCommand.py
-ba2731c 9 minutes ago
-61 lines (55 sloc)  2.24 KB
-    
 #! python3
-# putCommand.py - put command/commands from cmd.txt to device/ multiple devces.
+# putSpecificCommand.py - put specific command/commands from cmd file to specific devices.
+# cmd file & list of devices are in cmdRouter.csv.
 # Usage:
-#   python putCommand.py cmd.txt <device_hostname> - put command on a device, device must exist in my_testbed.yaml file
-#   python putCommand.py cmd.txt - put command on multiple devices in my_testbed.yaml file
+#   python putSpecificCommand.py cmdRouter.csv - put command on a device, device must exist in my_testbed.yaml file
 # my_testbed.yaml file must exist in current working directory
+# cmdRouter.csv file must exist in current working directory
+# Example of cmdRouter.csv:
+#  rustphrbtqfb-1.txt,rustphrbtqfb-1
+#  rustphrbtqfb-2.txt,rustphrbtqfb-2
 
 #Import neccessary libraries/modules
 import pickle
@@ -35,11 +21,12 @@ testbed = loader.load('my_testbed.yaml')
 genie_testbed = Genie.init(testbed)
 logfile = open('log.txt', 'w+')
 
-#Define readCommandFile()
-def readCommandFile(cmdFile):
-  with open(cmdFile) as f:
-    cmd = f.read().splitlines()
-  return cmd
+#Define readCommandRouterFile()
+def readCommandRouterFile(cmdRouterFile):
+  with open(cmdRouterFile) as f:
+    csvf = csv.reader(f)
+    data = list(csvf)
+  return data
   
 #Define putCommand()
 def putCommand(dev, cmdList):
@@ -50,41 +37,25 @@ def putCommand(dev, cmdList):
   device.disconnect()
 
 #Main program
-if len(sys.argv) < 3:                                       #If user do not enter <device_hostname>
-    print('Put command on multiple devices from my_testbed.yaml file'+'\n')
-    cmdFile = sys.argv[1]
-    cmdList = readCommandFile(str(cmdFile))
-    for dev in genie_testbed.devices.keys():
-        print('Put command on ' + str(dev) + '\n')
-        logfile.write('Put command on ' + str(dev) + '\n')
-        try:
-                putCommand(dev, cmdList)
-                logfile.write('Put command on ' + str(dev) + '---DONE!!!' + '\n')
-        except:
-                print('Failed to establish connection to ' + str(dev) + '\n')
-                logfile.write('Failed to establish connection to ' + str(dev) + '\n')
-                continue
-    logfile.close()
-    sys.exit()
-#If user do not enter <device_hostname> -- len(sys.argv) = 3, start from 0, end at 2
-cmdFile = sys.argv[1]                                        #Store 1st sys argument to cmdFile var
-dev = sys.argv[2]                                            #Store 2nd sys argument to dev var
-print('Put command on ' + str(dev) + '\n')
-logfile.write('Put command on ' + str(dev) + '\n')
-cmdList = readCommandFile(str(cmdFile))
-putCommand(dev, cmdList)
-logfile.write('Put command on ' + str(dev) + '---DONE!!!' + '\n')
+if len(sys.argv) < 2:                                       #If user do not enter cmdRouter.csv
+   print('Please input the cmdRouter.csv file')
+   print('Usage: python putSpecificCommand.py cmdRouter.csv') 
+   sys.exit()
+#If user do enter cmdRouter.csv
+cmdRouterFile = sys.argv[1]                                  #Store 1st sys argument to cmdRouterFile var
+data = readCommandRouterFile(cmdRouterFile)
+for i in range (1,len(data),1):                              #len(data) = number of rows/ devices in csv file
+   cmdFile = data[i][0]                                      #cdmFile in column 0
+   dev = data[i][1]                                          #dev in column 1                                            
+   print('Put command on ' + str(dev) + '\n')
+   logfile.write('Put command on ' + str(dev) + '\n')
+   cmdList = readCommandFile(str(cmdFile))
+   try:
+      putCommand(dev, cmdList)
+      logfile.write('Put command on ' + str(dev) + '---DONE!!!' + '\n')
+   except:
+      print('Failed to establish connection to ' + str(dev) + '\n')
+      logfile.write('Failed to establish connection to ' + str(dev) + '\n')
+      continue
 logfile.close()
 sys.exit()
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
