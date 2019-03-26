97e39c4461ab4ed7a249dd5afd2495eff94d15ca
linhht <31060606+linhht@users.noreply.github.com>
2019-03-26 11:17:32 +0700

    Update getConfigMultiDevice.py

--- getConfigMultiDevice.py
+++ getConfigMultiDevice.py
@@ -1,53 +1,38 @@
 #! python3
-# get_config.py - get config of device/devices & write to file/files.
-# get_config.py usage:
-# python get_config.py - get config of multiple devices from my_testbed.yaml file
-# python get_config.py <device_hostname> - get config of a specific device, device must exist in my_testbed.yaml file
+# getConfigMultiDevice.py - get config of devices & write to file/files.
+# getConfigMultiDevice.py usage:
+# python getConfigMultiDevice.py - get config of multiple devices from my_testbed.yaml file
+# my_testbed.yaml file should be in current working directory
 # remember to creat 'config' folder before running script
 
-############################## Import neccessary libraries/modules ###############################
+#Import neccessary libraries/modules
 import pickle
 import sys
 import pprint
 from ats.topology import loader
 from genie.conf import Genie
+from getConfigSingleDevice import getconfig
 
 #define global variables
 testbed = loader.load('my_testbed.yaml')
 genie_testbed = Genie.init(testbed)
 logfile = open('log.txt', 'w+')
 
-# python get_config.py - get config of multiple devices from my_testbed.yaml file
-if len(sys.argv) < 2:
-    print('Get config of multiple devices from my_testbed.yaml file')
-    for device in genie_testbed.devices.values():
-        print('Get config of ' + str(device) + '\n')
-        logfile.write('Get config of ' + str(device) + '\n')
-        try:
-            device.connect()
-            output_pre = device.parse('show running-config')
-            with open('config/running-config_' + str(device.name) + '.txt', 'wb') as f:
-                pickle.dump(output_pre, f)
-            f.close()
-            logfile.write('Get config of ' + str(device) + '---DONE!!!' + '\n')
-            device.disconnect()
-        except:
-            print('Failed to establish connection to ' + str(device) + '\n')
-            logfile.write('Failed to establish connection to ' + str(device) + '\n')
-            continue
-    logfile.close()
-    sys.exit()
-# python get_config.py <device_hostname> - get config of a specific device, device must exist in my_testbed.yaml file
-dev = ' '.join(sys.argv[1:])
-print('Get config of ' + str(dev) + '\n')
-logfile.write('Get config of ' + str(dev) + '\n')
-device = genie_testbed.devices[str(dev)]
-device.connect()
-output_pre = device.parse('show running-config')
-with open('config/running-config_' + str(device.name) + '.txt', 'wb') as f:
-    pickle.dump(output_pre, f)
-f.close()
-logfile.write('Get config of ' + str(dev) + '---DONE!!!' + '\n')
+#Main program
+if len(sys.argv) > 2:                                       #If user do enter more than one argument
+        print('You do not need to input the argument')
+        print('Usage: python python getConfigMultiDevice.py')
+        sys.exit()
+print('Get config of multiple devices from my_testbed.yaml file')
+for device in genie_testbed.devices.values():
+    print('Get config of ' + str(device) + '\n')
+    logfile.write('Get config of ' + str(device) + '\n')
+    try:
+        getconfig(device)
+    except:
+        print('Failed to establish connection to ' + str(device) + '\n')
+        logfile.write('Failed to establish connection to ' + str(device) + '\n')
+        continue
 logfile.close()
-device.disconnect()
 sys.exit()
+
