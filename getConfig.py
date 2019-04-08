f8d10b6fee9558aba791d0a0da0555f161fc8a67
linhht <31060606+linhht@users.noreply.github.com>
2019-04-08 11:22:56 +0700

    Update getConfig.py

--- getConfig.py
+++ getConfig.py
@@ -21,7 +21,7 @@ logfile = open('log.txt', 'w+')
 #Define getconfig() function
 def getconfig(dev):
     device = genie_testbed.devices[str(dev)]                #Get device from genie_testbed var
-    device.connect()                                        #Connect to device
+    device.connect(learn_hostname=True)                                        #Connect to device
     output_pre = device.parse('show running-config')        #Parse 'show run' cmd & store to outout_pre structure object
     with open('config/running-config_' + str(device.name) + '.txt', 'wb') as f:
         pickle.dump(output_pre, f)                          #Write outout_pre structure object to txt file use pickle
